7d87cc1ab5a65dd6da52eb87a887ce686924a01a
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-08-21 15:39:23 -0400

    Starter Files, Initial Components

---
+++ src/components/MainPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div>
+      Default View. 
+      </div>
+    );
+  }
+}
+
+export default MainPage;
