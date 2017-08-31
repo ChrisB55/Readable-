680ae4d8148f86164bad7f8e9741a073a417519c
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-08-30 20:06:43 -0400

    Match to server.js calls for project
    Partially returns desired data.

--- src/ReadableAPI.js
+++ src/ReadableAPI.js
@@ -1,42 +1,32 @@
 const api = "http://localhost:5001"; 
 
-
 let token = localStorage.token
 if (!token)
   token = localStorage.token = Math.random().toString(36).substr(-8)
 
 const headers = {
   'Accept': 'application/json',
-  'Authorization': token
+  'Authorization': token,
+  'Content-Type': 'application/json'
 }
 
-export const get = (bookId) =>
-  fetch(`${api}/books/${bookId}`, { headers })
+export const getCategory = () =>
+  fetch(`${api}/categories`, { headers })
     .then(res => res.json())
-    .then(data => data.book)
+    .then(data => console.log("getCategory", data)) 
 
-export const getAll = () =>
-  fetch(`${api}/books`, { headers })
+export const getPost= () =>
+  fetch(`${api}/posts`, { headers })
     .then(res => res.json())
-    .then(data => data.books)
+    .then(data => data.posts)
 
-export const update = (book, shelf) =>
-  fetch(`${api}/books/${book.id}`, {
-    method: 'PUT',
-    headers: {
-      ...headers,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ shelf })
-  }).then(res => res.json())
+export const getCategoryPosts = () =>
+    fetch(`${api}/category/posts`, { headers })
+      .then(res => res.json())
+      .then(data => data.getByCategory)
 
-export const search = (query, maxResults) =>
-  fetch(`${api}/search`, {
-    method: 'POST',
-    headers: {
-      ...headers,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ query, maxResults })
-  }).then(res => res.json())
-    .then(data => data.books)
+export const getComments = () =>
+      fetch(`${api}/comments`, { headers })
+        .then(res => res.json())
+        .then(data => data.comments)
+
