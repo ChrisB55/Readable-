42ae3f29aadd2d70ea25a61b4824162b496d5b4e
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-09-01 14:05:18 -0400

    Add initial method
    Testing API calls

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import MainPage from "./components/MainPage";
 import Header from "./components/Header";
 import Category from "./components/Category";
 import CreateNew from "./components/CreateNew";
-
+import * as ReadableAPI from "./ReadableAPI";
 import { BrowserRouter as Router, Route,} from "react-router-dom";
 
 class App extends Component {
+  componentDidMount() {
+    ReadableAPI.getCategory().then(categories => {
+      this.setState({categories}); 
+      console.log(categories)
+    })
+  }
+  
+  
   render() {
     return (
       <div className="App">
